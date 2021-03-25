9a242c15267708b3989f551e2b8564bd72de633b
karigar <abhishek.karigar@nokia.com>
2021-03-26 00:27:21 +0530

    first commit

---
+++ src/com/cluster/Constants.groovy
@@ -0,0 +1,6 @@
+package com.cluster
+
+class Constants {
+    public static final String SLACK_MESSAGE = 'Sending Slack Notification'
+    public static final String EMAIL_MESSAGE = 'Sending Email'
+}
