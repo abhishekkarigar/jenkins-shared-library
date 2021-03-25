637d15a81d6e0b361b51ef25ff72c308dde4754a
abhishekkarigar <a.karigar004@gmail.com>
2021-03-26 02:12:11 +0530

    Update dockerCredentials.groovy

--- vars/dockerCredentials.groovy
+++ vars/dockerCredentials.groovy
@@ -1,11 +1,13 @@
 def call(String appName, String buildNo) {
-    withCredentials([usernamePassword(
-            credentialsId: "docker",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )]){
-        sh "docker login -u $USER -p $PASS"
-        sh "docker build -t $USER/$appName:$buildNo ."
-        sh "docker push $USER/$appName:$BUILD_NUMBER"
+    script {
+        withCredentials([usernamePassword(
+           credentialsId: "docker",
+           usernameVariable: "USER",
+           passwordVariable: "PASS"
+        )]){
+          sh "docker login -u $USER -p $PASS"
+          sh "docker build -t $USER/$appName:$buildNo ."
+          sh "docker push $USER/$appName:$BUILD_NUMBER"
+        }
     }
 }
